ce8ac731ae1879f478ba25ac3aec871c1305cac4
sai-png <67268899+sai-png@users.noreply.github.com>
2022-03-22 23:51:40 +0530

    Time: 8 ms (83.49%), Space: 7.5 MB (80.12%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> dp(n);
+        int two = 0; // Maintain the three pointers i.e. two , three , five
+        int three = 0;
+        int five = 0;
+        
+        dp[0] = 1;
+        for(int i = 1 ; i < n ; i++){
+            int two_multiple = 2 * dp[two];
+            int three_multiple = 3 * dp[three];
+            int five_multiple = 5 * dp[five];
+            
+            dp[i] = min(two_multiple , min(three_multiple , five_multiple)); 
+            if(dp[i] == two_multiple){
+                two++;
+            }
+            if(dp[i] == three_multiple){
+                three++;
+            }
+            if(dp[i] == five_multiple){
+                five++;
+            }
+        }
+        return dp[n - 1];
+    }
+};

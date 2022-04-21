337c0a4ee03ea0a4cc061caec51af27fcad30bff
sai-png <67268899+sai-png@users.noreply.github.com>
2022-04-21 21:59:57 +0530

    Time: 11 ms (25.23%), Space: 9.1 MB (5.26%) - LeetHub

---
+++ 942-di-string-match/942-di-string-match.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> res;
+
+        for (int i = 0, l = 0, r = s.size(); i <= s.size(); i++)
+        {
+            res.push_back(i == s.size() || s[i] == 'I' ? l++:r--);
+        }
+        return res;
+    }
+};

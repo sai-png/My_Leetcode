076ed395d1659f285249ad0605a020473ee8f1a8
sai-png <67268899+sai-png@users.noreply.github.com>
2022-04-21 22:00:35 +0530

    Time: 27 ms (43.64%), Space: 13.4 MB (28.72%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int countKDifference(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
-        for (int &num : nums) {
-            m[num]++;
-        }
-        
-        int res = 0;
-        for (auto& pair : m) {
-            if(m.find(pair.first + k) != m.end())
-                res += pair.second*m[pair.first + k];
+    vector<int> diStringMatch(string s) {
+        vector<int> res;
+
+        for (int i = 0, l = 0, r = s.size(); i <= s.size(); i++)
+        {
+            res.push_back(i == s.size() || s[i] == 'I' ? l++:r--);
         }
         return res;
     }

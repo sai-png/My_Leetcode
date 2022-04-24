b93afaedda0fefb41d82f0a395357469f8f96565
sai-png <67268899+sai-png@users.noreply.github.com>
2022-04-24 22:35:34 +0530

    Time: 20 ms (64.18%), Space: 13.6 MB (70.67%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+   int minimumDifference(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    int ans = 100000;
+    for(int i=k-1;i<nums.size();++i)
+        ans = min(ans, nums[i]-nums[i-k+1]);
+    return ans;
+}
+};

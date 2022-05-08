7fc9f04971be140df143698aec5a3f9d5c2dfecd
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-08 22:19:23 +0530

    Time: 30 ms, Memory: 9.6 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.cpp
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==0){
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i]==0)
+            {
                 arr.pop_back();
                 arr.insert(arr.begin()+i,0);
-                i++;                                //This increment used to avoid checking the inserted zero.
+                i++;
             }
         }
+        
     }
 };

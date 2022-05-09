10f2930952adf3029fe08f08a28a29b16886a364
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-09 12:22:21 +0530

    Time: 35 ms, Memory: 9.6 MB - LeetHub

--- duplicate-zeros/duplicate-zeros.cpp
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -3,13 +3,13 @@ public:
     void duplicateZeros(vector<int>& arr) {
         for(int i=0;i<arr.size();i++)
         {
-            if(arr[i]==0)
-            {
-                arr.pop_back();
-                arr.insert(arr.begin()+i,0);
-                i++;
+          if(arr[i]==0)
+          {
+              arr.pop_back();
+              arr.insert(arr.begin()+i,0);
+              i++;
+          }
             }
-        }
         
     }
 };

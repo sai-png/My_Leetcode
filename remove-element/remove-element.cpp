e00a9a6873a47fd9887e7621afa75442b0c83e3d
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-12 20:46:56 +0530

    Time: 0 ms, Memory: 8.7 MB - LeetHub

--- remove-element/remove-element.cpp
+++ remove-element/remove-element.cpp
@@ -4,8 +4,7 @@ public:
         int count=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=val){
-                nums[count]=nums[i];
-                count++;
+                nums[count++]=nums[i];
             }
         }
         return count;

dca73bb63d53d0ca117815a339ca84e28ae93442
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-16 17:59:31 +0530

    Time: 26 ms, Memory: 22.5 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.cpp
+++ valid-mountain-array/valid-mountain-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int low=0,peak=0,plain=0;
+        int low=0,peak=0;
         if(arr.size()<3 ) return false;
         for(int i=1;i<arr.size()-1;i++)
         {
@@ -12,11 +12,8 @@ public:
             if(arr[i-1]<arr[i] and arr[i+1]<arr[i] ){
                 peak++;
             }
-             if(arr[i-1]==arr[i] and arr[i+1]==arr[i] ){
-                plain++;
-            }
         }
-       if(low==0 and peak==1 and plain==0)
+       if(low==0 and peak==1 )
            return true;
         return false;
     }

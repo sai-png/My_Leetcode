ce056ae30fee3ffb5078bd56e569a51fc429ebe3
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-17 13:36:13 +0530

    Time: 8 ms, Memory: 9.4 MB - LeetHub

--- third-maximum-number/third-maximum-number.cpp
+++ third-maximum-number/third-maximum-number.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int thirdMax(vector<int>& a) {
-      vector<int> ans;
-        int n=a.size();
-        sort(a.rbegin(),a.rend());//sorting it in descending order
-       ans.push_back(a[0]);
+    int thirdMax(vector<int>& nums) {
+        vector<int>ans;
+        int n=nums.size();
+        sort(nums.rbegin(),nums.rend());
+        ans.push_back(nums[0]);
         for(int i=1;i<n;i++){
-            if(a[i]!=a[i-1]){
-                ans.push_back(a[i]);
+            if(nums[i]!=nums[i-1])
+            {
+                ans.push_back(nums[i]);
             }
         }
         if(ans.size()<3){
             return ans[0];
         }
         else return ans[2];
-        
     }
 };

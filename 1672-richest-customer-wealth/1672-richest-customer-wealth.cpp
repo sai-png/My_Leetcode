bede83373ea03ec169bd8e3c79de8da6654fa24e
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-18 16:14:00 +0530

    Time: 10 ms (26.37%), Space: 7.9 MB (52.47%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int maxi=0;
-        int n=accounts.size(); //size of a row 
-        int m=accounts[0].size(); //size of a column
+       int count=0;
+        int n=accounts.size();
+        int m=accounts[0].size();
         for(int i=0;i<n;i++){
-             int sum=0;
+            int sum=0;
             for(int j=0;j<m;j++){
-                sum+=accounts[i][j];
-                maxi=max(sum,maxi);
-                
+              sum=sum+accounts[i][j];
+                count=max(sum,count);
             }
         }
-      return maxi;  
+     return count;
     }
 };

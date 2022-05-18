bbc4a37987b131fc885125096bb80e71159e3c0c
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-18 19:54:08 +0530

    Time: 11 ms (57.81%), Space: 14 MB (58.21%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -6,7 +6,7 @@ public:
             if(operations[i]=="++X" || operations[i]=="X++")
                 X++;
             else
-                --X;
+                X--;
         }
         return X;
     }

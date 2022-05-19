d3f52e855f0d71233c5fa4272e2c633177b410e8
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-19 12:29:18 +0530

    Time: 16 ms (51.88%), Space: 10.3 MB (17.41%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sentences) {
-        int ans=0;
+        int ans=0; int cnt;
         for(auto x: sentences){
-         ans=max(ans,(int)count(begin(x),end(x),' ')+1);
+            int cnt=(int)count(begin(x),end(x),' ')+1;
+         ans=max(ans,cnt);
                  }
         return ans;
     }

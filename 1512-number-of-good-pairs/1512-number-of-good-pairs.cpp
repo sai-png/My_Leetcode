b34f989032233f2fe34af9700caae5a963df1af7
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-22 18:00:30 +0530

    Time: 3 ms (51.88%), Space: 7.3 MB (60.91%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -5,9 +5,7 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
+                if(nums[i]==nums[j]) count++;
             }
         }
         return count;

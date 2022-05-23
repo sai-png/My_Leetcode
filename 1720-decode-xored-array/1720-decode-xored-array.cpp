79c54bd576b3145913d0592a4a8b3f15f3a39898
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-23 12:42:32 +0530

    Time: 38 ms (59.79%), Space: 26.1 MB (34.61%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
         vector<int>ans;
+        int x;
         ans.push_back(first);
         for(int i=0;i<encoded.size();i++){
-            int x=ans[i]^encoded[i];
+             x=ans[i]^encoded[i];
             ans.push_back(x);
         }
        return ans; 

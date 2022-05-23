39a75722b86593d224390d541948362841ac6ecf
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-23 09:58:50 +0530

    Time: 250 ms (82.28%), Space: 33.4 MB (55.24%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,36 +1,38 @@
+
+int static dp[602][102][102];
 class Solution {
 public:
-    int dp[101][101][601];
-    pair<int,int> checkZeroOne(string s,int m,int n){
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='0'){
-                m--;
-            }else{
-                n--;
-            }
+    pair<int,int> countOnesZeros(string s){
+        int zeros=0,ones=0;
+        for(auto i: s){
+            if(i=='0') zeros++;
+            if(i=='1') ones++;
         }
-        pair<int,int> p = make_pair(m,n);
-        return p;
+        return {zeros,ones};
     }
-    int solve(int k,vector<string>& strs,int m,int n){
-        if(k==0){
+    int findMax(vector<pair<int,int> > &vec,int size, int m , int n){
+        if(size==0 || (m==0 and n==0)){
             return 0;
         }
-        if(dp[m][n][k]!=-1){
-            return dp[m][n][k];
+        if(dp[size][m][n]!=-1){
+            return dp[size][m][n];
         }
-        pair<int,int> p = checkZeroOne(strs[k-1],m,n);
-        int remainingM = p.first;
-        int remainingN = p.second;
-        if(remainingM>=0 && remainingN>=0){
-            return dp[m][n][k] = max(1+solve(k-1,strs,remainingM,remainingN) , solve(k-1,strs,m,n));
+        if(vec[size-1].first<=m and vec[size-1].second<=n){
+            return dp[size][m][n] = max(1+ findMax(vec,size-1,m-vec[size-1].first,n-vec[size-1].second),findMax(vec,size-1,m,n));
+        }else{
+            return dp[size][m][n] = findMax(vec,size-1,m,n);
         }
-        else{
-            return dp[m][n][k] = solve(k-1,strs,m,n);
-        }
+        
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
+        int sz = strs.size();
+        int mx = 0;
+        
         memset(dp,-1,sizeof(dp));
-        return solve(strs.size(),strs,m,n);
+        vector<pair<int,int> > vec;
+        for(auto it: strs){
+            vec.push_back(countOnesZeros(it));
+        }
+        return findMax(vec, sz, m,n );
     }
 };

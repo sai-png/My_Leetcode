9c6c33dae43d40501b4d5781436e9063c5ed39b9
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-24 18:29:34 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (41.94%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -3,16 +3,14 @@ public:
     int sumOddLengthSubarrays(vector<int>& arr) {
         int sum=0;
         for(int i=0;i<arr.size();i++){
-            int sub=0;
-            for(int j=i;j<arr.size();j++){
-                sub+=arr[j];
-                if((j-i+1)%2!=0){
-                  sum+=sub;
-                }
-                
+          int sub=0;
+        for(int j=i;j<arr.size();j++){
+            sub+=arr[j];
+        if((j-i+1)%2!=0)
+            sum+=sub;
             }
         }
         return sum;
-        
     }
 };
+

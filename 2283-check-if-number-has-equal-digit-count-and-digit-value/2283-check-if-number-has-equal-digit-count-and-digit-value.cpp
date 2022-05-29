72cdf5b423180c9084ea4b1224159c29c5cc865f
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-29 16:31:29 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (20.00%) - LeetHub

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool digitCount(string num) {
+        unordered_map<char,int>ans;
+        
+        for(int i=0; i<num.size(); i++)
+            ans[num[i]]++;
+        for(int i=0; i<num.size(); i++){
+            char c ='0' + i;
+            if(num[i] != '0' + ans[c]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};

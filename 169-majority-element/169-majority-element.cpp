6a21809caeef6cb64101033f3d4b3e2a704bddec
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-05-30 17:57:11 +0530

    Time: 34 ms (23.53%), Space: 19.6 MB (14.36%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n=nums.size();
         sort(nums.begin(),nums.end());
-        return  nums[n/2];   
+        return  nums[nums.size()/2];   
     }
 };

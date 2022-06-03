a5e4781a61f1e7b55141d9335529365a74b9627d
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-03 19:52:12 +0530

    Time: 50 ms (8.20%), Space: 23.8 MB (12.48%) - LeetHub

--- 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
+++ 922-sort-array-by-parity-ii/922-sort-array-by-parity-ii.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    vector<int> sortArrayByParityII(vector<int>& nums) {
-        int n = nums.size();
-int i=0,j=1;
-sort(nums.begin(),nums.end());
-while(i<n and j<n)
-{
-if(nums[i]%2==1 and nums[j]%2==0)
-{
-swap(nums[i],nums[j]);
-}
-else if(nums[i]%2==0)
-{
-i+=2;
-}
-else if(nums[j]%2==1)
-{
-j+=2;
-}
-}
-return nums;
+    vector<int> sortArrayByParityII(vector<int>& nums) 
+    {
+       int n=nums.size();
+       vector<int> ev;
+        vector<int> ans;
+        vector<int> od;
+       for(int i=0;i<n;i++)
+       {
+           if(nums[i]%2==0)
+               ev.push_back(nums[i]);
+           else
+               od.push_back(nums[i]);
+       }
+        int j=0,k=0;
+       for(int i=0;i<n;i++)
+       {
+           if(i%2==0)
+           {
+               ans.push_back(ev[j]);
+               j++;
+           }
+           else
+           {
+               ans.push_back(od[k]);
+               k++;
+           }
+       }
+        return ans;
+        
+        
     }
 };

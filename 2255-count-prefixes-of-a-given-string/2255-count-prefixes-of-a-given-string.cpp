08d0802c20b4711841173b401f546ab1b95b3e89
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-03 09:36:56 +0530

    Time: 8 ms (84.77%), Space: 12 MB (26.95%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int countPrefixes(vector<string>& words, string s) {
-        int kount = 0;
-
-        for(auto& word : words){
-            if(word.length() > s.length()) continue;
-            auto res = std::mismatch(word.begin(), word.end(), s.begin());
-            kount += (res.first == word.end());
+        int n = words.size();
+        int len = s.size();
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            int cnt = 0;
+            for(int j=0;j<len;j++){
+                if(words[i][j]==s[j]){
+                    cnt++;
+                }
+                else break;
+            }
+            if(cnt==words[i].size()) ans++;
         }
-        return kount;       
+        return ans;
     }
 };

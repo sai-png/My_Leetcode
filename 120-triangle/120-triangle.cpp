b81b9b79b80ee485afdff59f1b434c104465d808
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-13 23:19:48 +0530

    Time: 3 ms (97.83%), Space: 8.8 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-   int minTotalUtil(vector<vector<int>>& triangle, int h, int j,vector<vector<int>> &v) {
-        if(h == triangle.size()) {
-             
-            return 0;
-        }
-         if(v[h][j]!=-1) return v[h][j];  
-        
-        return v[h][j]=triangle[h][j]+min(minTotalUtil(triangle, h+1, j,v),minTotalUtil(triangle, h+1, j+1,v));
+    int dp[205][205];//top down approach
+    int helper(int i,int j,vector<vector<int>> &nums){
+        if(i>=nums.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=0;
+        ans=nums[i][j]+min(helper(i+1,j,nums),helper(i+1,j+1,nums));
+        dp[i][j]=ans;
+        return ans;
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int minSum = INT_MAX;
-          int h=triangle.size(),j;
-         j=h;
-        vector<vector<int>> v(h,vector<int> (j,-1));
-          minSum=minTotalUtil(triangle, 0, 0,v);
-        
-        cout << minSum << endl;
-        return minSum;
-        
+    int minimumTotal(vector<vector<int>>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return helper(0,0,nums);
     }
 };

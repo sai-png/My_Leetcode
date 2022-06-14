82192808796d3a62ece17d025b8fe3f796799529
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-14 16:19:35 +0530

    Time: 41 ms (29.02%), Space: 10.8 MB (23.10%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,9 +3,7 @@ public:
 	int lengthOfLongestSubstring(string s) 
 	{
 		unordered_set<char> set;
-    
 		int i = 0, j = 0, n = s.size(), ans = 0;
-    
 		while( i<n && j<n)
 		{
 			if(set.find(s[j]) == set.end()) 

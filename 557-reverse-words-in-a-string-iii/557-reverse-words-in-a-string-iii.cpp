6cf61389926405afc6999205a691ef35696dac5d
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-15 18:40:46 +0530

    Time: 18 ms (61.73%), Space: 9.7 MB (45.27%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-string reverseWords(string s) {
-int j=0,i=0;
-for(;i<s.length();i++){
-if( s[i]==' '){
-reverse(s.begin()+j , s.begin()+i);
-j=i+1;}
-}
-reverse(s.begin()+j , s.begin()+i);
-return s;
-}
+    string reverseWords(string s) {
+        int n=s.length();
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(s[i]==' '){
+                reverse(s.begin()+j,s.begin()+i);
+                j=i+1;
+            }
+            
+        }
+        reverse(s.begin()+j,s.end());
+        return s;
+    }
 };

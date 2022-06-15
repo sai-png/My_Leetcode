0617656867651b93970593743c0e51b8c51ad2d4
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-15 19:56:59 +0530

    Time: 6 ms (70.95%), Space: 8.8 MB (67.15%) - LeetHub

--- 942-di-string-match/942-di-string-match.cpp
+++ 942-di-string-match/942-di-string-match.cpp
@@ -2,10 +2,24 @@ class Solution {
 public:
     vector<int> diStringMatch(string s) {
         vector<int> res;
-
-        for (int i = 0, l = 0, r = s.size(); i <= s.size(); i++)
-        {
-            res.push_back(i == s.size() || s[i] == 'I' ? l++:r--);
+        int n=s.length();
+        int i=0;
+        int j=n;
+        for(int k=0;k<n;k++){
+            if(s[k]=='I'){
+                res.push_back(i);
+                i++;
+            }
+            else if(s[k]=='D'){
+                res.push_back(j);
+                j--;
+            } 
+        }
+        if(s[s.size()-1]=='I'){
+            res.push_back(i);
+        }
+        else{
+            res.push_back(i);
         }
         return res;
     }

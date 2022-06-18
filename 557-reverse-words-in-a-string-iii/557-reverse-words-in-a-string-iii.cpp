e6c9c118a226799f67b4b547a7afcbc121c894c4
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-18 13:25:57 +0530

    Time: 26 ms (30.15%), Space: 9.7 MB (75.99%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     string reverseWords(string s) {
         int n=s.length();
-        int j=0;
+        int j=0; int i=0;
         for(int i=0;i<n;i++){
             if(s[i]==' '){
                 reverse(s.begin()+j,s.begin()+i);
                 j=i+1;
             }
-            
         }
-        reverse(s.begin()+j,s.end());
+        reverse(s.begin()+j,s.end()+i);
         return s;
     }
 };

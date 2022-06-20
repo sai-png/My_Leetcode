d54271b9d83c98a2355ee2aea25c8bdf6627b211
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-20 11:08:06 +0530

    Time: 33 ms (56.63%), Space: 19.2 MB (83.25%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-       int count=0;
-        for(int i=0;i<nums.size();i++){
+        int i=0;
+        int j=0;
+        int n=nums.size();
+        while(i<n){
             if(nums[i]!=0){
-                    swap(nums[i],nums[count++]);
-                
+                swap(nums[i],nums[j]);
+                i++;
+                j++;
+            }
+            else{
+                i++;
             }
         }
     }

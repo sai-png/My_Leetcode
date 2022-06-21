3a5b602d1dba35513c821c203196a27add329eb1
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-21 14:47:38 +0530

    Time: 148 ms (54.52%), Space: 53.9 MB (27.49%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    int furthestBuilding(vector<int>& nums, int bricks, int ladders) {
-        int n=nums.size();
-        multiset<int> s;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i-1]>=nums[i]) continue;
-            int d=nums[i]-nums[i-1];
-            if(d>0) s.insert(d);
-            if(s.size()>ladders){
-                bricks-=*s.begin();
-                s.erase(s.begin());
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+        int n = heights.size();
+        int totalBricksUsed = 0;
+        priority_queue<int> bricksUsed;
+        int reach = 0;
+        for (; reach < n-1; reach++) {
+            if (heights[reach] >= heights[reach+1]) {
+                continue;
             }
-            if(bricks<0) return i-1;
+            int diff = heights[reach+1] - heights[reach];
+        
+            if (totalBricksUsed + diff <= bricks) {
+                totalBricksUsed += diff;
+                bricksUsed.push(diff);
+            } else if (ladders > 0) {
+                if (!bricksUsed.empty() && bricksUsed.top() > diff) {
+                    totalBricksUsed = totalBricksUsed - bricksUsed.top() + diff;
+                    bricksUsed.pop();
+                    bricksUsed.push(diff);
+                }
+                ladders--;
+            } else {
+                break;
+            }
         }
-        return n-1;
+        return reach; 
     }
 };

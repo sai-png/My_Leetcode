dae7dfd6f91374781d9116c37f79a3933905fc2f
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-28 22:20:07 +0530

    Time: 8 ms (83.30%), Space: 14.2 MB (9.38%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
-        int x=0;
+        int ans=0;
         for(int i=0;i<operations.size();i++){
-            if(operations[i]=="--X" || operations[i]=="X--")
-                --x;
-            else
-                ++x;
+            if(operations[i]=="++X" or operations[i]=="X++"){
+                ans=ans+1;
+            }
+            else{
+                --ans;
+            }
         }
-        return x;
+        return ans;
     }
 };

c6ed2d9ab064ecf1b0b120ca25d04c7a0ad63420
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-06-29 13:56:55 +0530

    Time: 3 ms (61.78%), Space: 6 MB (95.40%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.cpp
+++ 771-jewels-and-stones/771-jewels-and-stones.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-    int numJewelsInStones(string jewels, string stones) {
-        map<char,int> jewels_count, stones_count;
-        int count=0;
-		
-        for (int i=0;i<jewels.size();i++){jewels_count[jewels[i]]++;}
-        for (int i=0;i<stones.size();i++){stones_count[stones[i]]++;}
-        map<char,int>::iterator itr;
-		
-        for (itr=stones_count.begin();itr!=stones_count.end();itr++){
-            if (jewels_count.find(itr->first)!=jewels_count.end()){
-                count += itr->second;
-            }
+    int numJewelsInStones(string j, string s) {
+        int ans=0;
+        for( char c: s){
+            if(j.find(c)!=string::npos)
+            ans++;
         }
-		
-        return count;
+        return ans;
     }
 };

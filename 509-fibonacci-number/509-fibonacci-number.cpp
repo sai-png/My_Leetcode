0f4391b663422a897395859eee91c1363e7148dd
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-06 06:29:40 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         if(n<=1) return n;
-        int prev2 = 0, prev1 = 1, curr;
+        int prev2 = 0, prev1 = 1, cur;
         for(int i=2;i<=n;i++){
-            curr = prev1+prev2;
+            cur = prev1+prev2;
             prev2 = prev1;
-            prev1 = curr;
+            prev1 = cur;
         }
-        return curr;
+        return cur;
     }
 };

fde2f93889c6eb40a9556974fea4d3ade86e0e43
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-20 23:09:32 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (34.90%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int>ans;
-        ans.push_back(nums[0]);
         for(int i=1;i<nums.size();i++){
             nums[i]=nums[i]+nums[i-1];
-            ans.push_back(nums[i]);
+           
         }
-        return ans;
+        return nums;
     }
 };

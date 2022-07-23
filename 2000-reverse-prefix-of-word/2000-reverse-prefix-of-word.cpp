d40c9dac61e4960e9f13115fa32453d569e3b5ba
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-24 00:46:02 +0530

    Time: 3 ms (47.98%), Space: 6.2 MB (31.68%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int j=0;
-      for(int i=0;i<word.size();i++){
-          if(word[i]==ch){
-              reverse(word.begin(),word.begin()+i+1);
-              break;
-          }
-      }
+        for(int  i=0;i<word.size();i++){
+            if(word[i]==ch){
+                reverse(word.begin(),word.begin()+i+1);
+                break;
+            }
+        }
         return word;
     }
 };

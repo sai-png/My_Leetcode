ffeac896038e0ad86b7df3e95d3e9152dda28130
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-24 00:20:47 +0530

    Time: 158 ms (5.04%), Space: 26.3 MB (6.25%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,21 +1,13 @@
 class Solution {
-    bool isPalindrome(string s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            if(s[i]!=s[j])
-                return false;
-                i++;
-                j--;
-        }
-          return true;
-    }
-    public:
+public:
     string firstPalindrome(vector<string>& words) {
+        string res;
+        string ans;
         for(int i=0;i<words.size();i++){
-            if(isPalindrome(words[i])){
-                return words[i];
-            }
+            string ans=words[i];
+            string res=ans;
+            reverse(res.begin(),res.end());
+            if(ans==res) return ans;
         }
         return "";
     }

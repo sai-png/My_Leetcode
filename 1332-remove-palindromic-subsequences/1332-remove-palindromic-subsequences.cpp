b2ee14d6a4e739cedae82add8cf3cda320af4e87
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-24 02:01:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (74.95%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removePalindromeSub(string s) {
-        int i=0;
+       int i=0;
         int j=s.length()-1;
         while(i<=j){
             if(s[i]!=s[j])
                 return 2;
-            i++;
-            j--;
+                i++;
+                j--;
         }
         return 1;
     }

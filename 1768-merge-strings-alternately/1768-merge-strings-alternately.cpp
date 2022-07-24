2f4815786286443b5f3a55cdb943566fc71b4229
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-24 11:59:42 +0530

    Time: 9 ms (5.36%), Space: 6.1 MB (96.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-     int i=0,j=0,m=word1.size(),n=word2.size();
+        int i=0;
+        int j=0;
+        int m=word1.size();
+        int n=word2.size();
         string ans="";
         while(i<m && j<n){
             ans+=word1[i++];
             ans+=word2[j++];
-        }
+}
         while(i<m){
-            ans+=word1[i++];
-        }
+            ans+=word1[i];
+            i++;
+}
         while(j<n){
-            ans+=word2[j++];
+            ans+=word2[j];
+            j++;
         }
         return ans;
     }

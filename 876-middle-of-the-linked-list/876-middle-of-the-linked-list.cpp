6f97d2f97c282d9515837c2e28b591a9399c26f9
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-07-29 23:33:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (21.76%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -13,12 +13,10 @@ public:
     ListNode* middleNode(ListNode* head) {
         ListNode* slow=head;
         ListNode* fast=head;
-       
         while(fast!=NULL and fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
         return slow;
-        
     }
 };

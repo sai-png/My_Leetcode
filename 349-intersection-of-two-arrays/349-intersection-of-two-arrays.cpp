b65783ad7e88a921a85e6ca665a90a8abde3981a
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-08-04 01:38:21 +0530

    Time: 24 ms (10.82%), Space: 10.2 MB (63.78%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-    set<int> st;        
-    for(int itr = 0; itr < nums1.size(); itr++){
-        for(int jtr = 0; jtr < nums2.size(); jtr++){
-            if(nums1[itr] == nums2[jtr]){
-                st.insert(nums1[itr]);
-                break;
+        set<int>st;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]){
+                    st.insert(nums1[i]);
+                    break;
+                }
             }
         }
+        vector<int>ans;
+        for(auto i=st.begin();i!=st.end();i++){
+            ans.push_back(*i);
+        }
+        return ans;
     }
-    
-    vector<int> vt;
-    for(auto itr = st.begin(); itr != st.end(); itr++){
-        vt.push_back(*itr);
-    }
-    return vt;
-}
 };

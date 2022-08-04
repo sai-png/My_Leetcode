35a85331d78a9bc215e11b123d55d16ba518bff0
Sai_Octobat <67268899+sai-png@users.noreply.github.com>
2022-08-04 15:51:12 +0530

    Time: 3 ms (36.36%), Space: 6 MB (26.45%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.cpp
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    int mirrorReflection(int p, int q) {
-        while (p % 2 == 0 && q % 2 == 0){ 
-            p/=2;
-            q/=2;
+    int mirrorReflection(int a, int b) {
+        while (a % 2 == 0 and b % 2 == 0){ 
+            a/=2;
+            b/=2;
         }
-        return 1 - p % 2 + q % 2;
+        int ans=1 - a % 2 + b % 2;
+        return ans;
     }
 };
